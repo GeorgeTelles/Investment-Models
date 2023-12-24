54e0729e9cae637b46ee153613bdf51ffed6ba2c
George Telles <george_1@hotmail.com.br>
2023-12-24 16:13:29 -0300

    Update EMA INTRA Lista.py

--- EMA INTRA Lista.py
+++ EMA INTRA Lista.py
@@ -10,17 +10,23 @@ if not mt5.initialize():
 # Lista de ativos
 #ativos = ["PETR4",]
 
-df_empresas = pd.read_excel(r'C:\Users\George Telles\Desktop\Pendencias\acoesfiltradas.xlsx', header=0, usecols="A")
+df_empresas = pd.read_excel(r'G:\\Meu Drive\\3. Finanças e Investimentos\\Códigos e Scripts\\acoesfiltradas.xlsx', header=0, usecols="A")
 ativos = df_empresas['Codigo'].tolist()
 
 performance_metrics = []
-PERIODO = 9
+PERIODO = 21
+trade_durations = []
+average_duration = []
+data_inicial = datetime.now()
+data_final = datetime(2018, 1, 1)
+time_frame = "M15"
+timeframe_enum = eval("mt5.TIMEFRAME_" + time_frame)
+
 
 for ativo in ativos:
     try:
-        data_inicial = datetime.now()
-
-        rates = mt5.copy_rates_from(ativo, mt5.TIMEFRAME_H1, data_inicial, 8000000)
+        
+        rates = mt5.copy_rates_range(ativo, timeframe_enum, data_final, data_inicial)
  
         rates_frame = pd.DataFrame(rates)
         rates_frame['time'] = pd.to_datetime(rates_frame['time'], unit='s')
@@ -75,13 +81,17 @@ for ativo in ativos:
                 trade_records.append({
                     'Entry_Date': entry_date,
                     'Exit_Date': exit_date,  # Deve ser igual a exit_index
-                    'Profit': strategy_return
+                    'Profit': strategy_return,
+                    'Duration': exit_index - entry_index
                 })
+                trade_durations.append(exit_index - entry_index)  # Adicionar a duração ao final da lista
                 rates_frame.at[index, 'Strategy_Return'] = strategy_return
+                
 
 
 
         # Imprima os retornos acumulados da estratégia
+        average_duration = sum(trade_durations) / len(trade_durations) if trade_durations else 0
         total_return = rates_frame['Strategy_Return'].sum()
         total_trades = len(trade_records)
         winning_trades = len([trade for trade in trade_records if trade['Profit'] > 0])
@@ -98,15 +108,16 @@ for ativo in ativos:
 
         performance_metrics.append({
             'Ativo': ativo,
-            'Retorno total da estratégia': total_return * 100,
-            'Total de operações': total_trades,
-            'Operações vencedoras': winning_trades,
-            'Operações perdedoras': losing_trades,
-            'Percentual de operações vencedoras': winning_trades / total_trades * 100,
-            'Média de ganho por operação': average_gain * 100,
-            'Média de perda por operação': average_loss * 100,
-            'Retorno médio por operação': average_profit_per_trade * 100,
-            'Retorno usando Buy and Hold': buy_and_hold_return,
+            'Retorno': total_return * 100,
+            'Total de op': total_trades,
+            'Op vencedoras': winning_trades,
+            'Op perdedoras': losing_trades,
+            'Percentual de op vencedoras': winning_trades / total_trades * 100,
+            'Média de ganho por op': average_gain * 100,
+            'Média de perda por op': average_loss * 100,
+            'Retorno médio por op': average_profit_per_trade * 100,
+            'Retorno BuyandHold': buy_and_hold_return,
+            'Média de Duração': average_duration 
         })
 
         # Imprimir informações sobre o desempenho da estratégia
@@ -131,4 +142,4 @@ for ativo in ativos:
 performance_df = pd.DataFrame(performance_metrics)
 
 # Exportar DataFrame para Excel
-performance_df.to_excel(f"EMA_{PERIODO}_INTRA_H1.xlsx", index=False)
+performance_df.to_excel(f"EMA_{PERIODO}_INTRA_{time_frame}.xlsx", index=False)
